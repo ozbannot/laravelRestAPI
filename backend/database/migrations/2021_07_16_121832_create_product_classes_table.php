f0b95df3adcffce5737daaadd6c87ca8a6cbe38d
ozbannot <ozbannot@gmail.com>
2021-07-16 21:34:40 +0900

    create ProductClassTable and fix Model

--- backend/database/migrations/2021_07_16_121832_create_product_classes_table.php
+++ backend/database/migrations/2021_07_16_121832_create_product_classes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTest1Table extends Migration
+class CreateProductClassesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,15 @@ class CreateTest1Table extends Migration
      */
     public function up()
     {
-        Schema::create('test1', function (Blueprint $table) {
+        Schema::create('product_classes', function (Blueprint $table) {
             $table->id();
             $table->integer('product_id'); // 商品ID
-            $table->integer('ga_id'); // gaID
-            $table->integer('login_id')->charset(null)->nullable(); // ログインID
+            $table->integer('stock'); // 在庫数
+            $table->tinyInteger('stock_limit_flag')->default(0); // 在庫制限フラグ
+            $table->integer('tax'); // 税率
+            $table->dateTime('publication_date'); // 掲載開始日時
+            $table->dateTime('from_date'); // 販売開始日時
+            $table->dateTime('to_date'); // 販売終了日時
             $table->dateTime('create_date'); // 作成日時
             $table->dateTime('update_date'); // 更新日時
             $table->tinyInteger('delete_flag')->default(0); // 削除フラグ
@@ -31,6 +35,6 @@ class CreateTest1Table extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('test1');
+        Schema::dropIfExists('product_classes');
     }
 }
