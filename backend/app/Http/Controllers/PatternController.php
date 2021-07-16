9c686173cffba0e9d0d56f78f5fc3996e7ac8382
ozbannot <ozbannot@gmail.com>
2021-07-16 22:31:12 +0900

    fix PatternController

--- backend/app/Http/Controllers/PatternController.php
+++ backend/app/Http/Controllers/PatternController.php
@@ -18,22 +18,47 @@ class PatternController extends Controller
     * @param int パターン
     * @return json レスポンス返却値
     */
-    public function index(Request $request, int $pattern)
+    public function index(Request $request,int $pattern)
     {
         // 初期定義
-        $response = [];
-        // テスト1テーブル情報取得
-        $test1TableInfo = $this->pattern->getTest1TableInfo();
-        // テスト2テーブル情報取得
-        $test2TableInfo = $this->pattern->getTest2TableInfo();
-        // json返却テストのため格納する
-        $response['pattern'] = $pattern;
-        $response['login_id'] = $test1TableInfo->login_id;
-        $response['ga_id'] = $test1TableInfo->ga_id;
-        $response['product_id_list'] = [];
-        $response['product_id_list']['product_id'] = $test2TableInfo->product_id;
-        $response['error_list'] = [];
+        $response = []; // レスポンス
+        $loginId = $request->login_id ?: null; // ログインID
+        $gaId = $request->ga_id ?: null; // GAID
+        $limit = $request->limit ?: null; // 取得数
+
+        // パターン1,3
+        if ($pattern === 1 || $pattern === 3) { // TODO ハードコーディング修正
+            // テスト1テーブル情報取得
+            $test1TableInfo = $this->pattern->getTest1TableInfo();
+            // json返却のため整形する
+            $response = $this->convertResponseData($pattern,$test1TableInfo);
+        }
+
+        // パターン2
+        if ($pattern === 2) {
+            // テスト2テーブル情報取得
+            $test2TableInfo = $this->pattern->getTest2TableInfo();
+            // json返却のため整形する
+            $response = $this->convertResponseData($pattern,$test2TableInfo);
+        }
 
         return response()->json($response);
+    }
+
+    /**
+    * レスポンスデータ整形
+    * @param int $pattern
+    * @param object $tableInfo
+    * @return array $response
+    */
+    public function convertResponseData($pattern,$tableInfo)
+    {
+        $response['pattern'] = $pattern;
+        $response['login_id'] = $tableInfo->login_id;
+        $response['ga_id'] = $tableInfo->ga_id;
+        $response['product_id_list'] = [];
+        $response['product_id_list']['product_id'] = $tableInfo->product_id;
+        $response['error_list'] = [];
+        return $response;
     }
 }
